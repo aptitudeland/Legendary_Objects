11fd04cdf25413c50fdae504a915c01e787a4bf6
Aptitudeland <jb.garnier@gmail.com>
2024-05-28 17:21:41 +0200

    Added Status column to table Reservation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_28_110209) do
+ActiveRecord::Schema[7.1].define(version: 202405281517120) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -32,6 +32,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_28_110209) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status", default: "Pending"
     t.index ["legend_id"], name: "index_reservations_on_legend_id"
     t.index ["user_id"], name: "index_reservations_on_user_id"
   end
