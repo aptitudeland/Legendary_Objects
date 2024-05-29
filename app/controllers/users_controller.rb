b6360835c21c75f227088f26785147e0d755143f
helene berthebaud <arretgros.helene@gmail.com>
2024-05-29 12:54:54 +0200

    usershow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,8 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     # la méthode show doit passer un array de réservation
     @reservations = @user.reservations # ça donne toutes les réservations EMISES par @user
-    
     # un array de legendes à la vue
+    @legends = @user.legends
   end
 
 
