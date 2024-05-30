1b7c5ad851d509373e30bfe96e4c5172b2555584
Aptitudeland <jb.garnier@gmail.com>
2024-05-30 16:33:52 +0200

    ReservationController Back

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,5 @@
 class Reservation < ApplicationRecord
   belongs_to :legend
   belongs_to :user
+  enum status: { pending: 0, confirmed: 1, canceled: 2, declined: 3 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,11 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
   resources :legends, only: %i[index show new create destroy]
-  resources :reservations
+  resources :reservations do
+    member do
+      patch :update_status
+    end
+  end
   # Defines the root path route ("/")
   # root "posts#index"
 end
